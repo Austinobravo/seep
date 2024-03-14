8ae836b8687b1f1a14653f641211f2b7bf4caa9a
Austinobravo <Austinobravo@gmail.com>
2024-03-14 11:42:51 +0100

    created news page

---
+++ app/(news)/news/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NewsHero from './_components/NewsHero'
+
+const page = () => {
+  return (
+    <div>
+        <NewsHero/>
+      
+    </div>
+  )
+}
+
+export default page
