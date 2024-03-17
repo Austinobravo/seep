4229cdc6ba705bd59691586364966c0311a09395
Austinobravo <Austinobravo@gmail.com>
2024-03-17 18:54:04 +0100

    added animation in team

--- app/(team)/team/_components/TeamRoles.tsx
+++ app/(team)/team/_components/TeamRoles.tsx
@@ -1,3 +1,4 @@
+import FadeInSection from '@/hooks/fadeIn'
 import Image from 'next/image'
 import React from 'react'
 const team = [
@@ -35,22 +36,24 @@ const team = [
 const TeamRoles = () => {
   return (
     <section className='md:px-20 px-10 py-10'>
-        <div className='grid md:grid-cols-3 grid-cols-1 gap-10'>
-            {team.map((member, index) => (
-                <div key={index} className='seep-text-color'>
-                    <div className='seep-bg-color rounded-2xl h-52'>
-                        <span/>
-                        {/* <Image src={member.image} width={100} height={100} alt={member.name}/> */}
-                    </div>
-                    <div>
-                        <h3 className='text-2xl font-bold'>{member.name}</h3>
-                        <p>{member.role}</p>
-                    </div>
+        <FadeInSection direction={`up`}>
+            <div className='grid md:grid-cols-3 grid-cols-1 gap-10'>
+                {team.map((member, index) => (
+                    <div key={index} className='seep-text-color'>
+                        <div className='seep-bg-color rounded-2xl h-52'>
+                            <span/>
+                            {/* <Image src={member.image} width={100} height={100} alt={member.name}/> */}
+                        </div>
+                        <div>
+                            <h3 className='text-2xl font-bold'>{member.name}</h3>
+                            <p>{member.role}</p>
+                        </div>
 
-                </div>
-            ))}
+                    </div>
+                ))}
 
-        </div>
+            </div> 
+        </FadeInSection>
       
     </section>
   )
