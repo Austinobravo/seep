3815d6b22477383f1ced29b66ded66b0adfc8479
Austinobravo <Austinobravo@gmail.com>
2024-03-17 18:57:58 +0100

    added animation in contact

--- app/(contact)/contact/_components/ContactHero.tsx
+++ app/(contact)/contact/_components/ContactHero.tsx
@@ -1,13 +1,16 @@
+import FadeInSection from '@/hooks/fadeIn'
 import React from 'react'
 
 const ContactHero = () => {
   return (
     <section className='md:px-20 px-10'>
+      <FadeInSection direction={`up`}>
         <div className='w-full bg-cover md:bg-right-top bg-center h-[600px] rounded-2xl text-white' style={{backgroundImage: `url(/images/mission_2.png)`}}>
             <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent h-[600px] rounded-2xl flex flex-col md:pl-10 pl-5 md:justify-center justify-end pb-16 space-y-5'>
                 <h1 className='md:text-6xl text-3xl  leading-relaxed md:w-[600px]'>Contact Us.</h1>
             </div>
-        </div>
+        </div>   
+      </FadeInSection>
     </section>
   )
 }
--- app/(testimonials)/testimonials/_components/TestimonialSection.tsx
+++ app/(testimonials)/testimonials/_components/TestimonialSection.tsx
@@ -1,3 +1,4 @@
+import FadeInSection from '@/hooks/fadeIn'
 import Image from 'next/image'
 import React from 'react'
 const Testimonials = [
@@ -23,24 +24,26 @@ const Testimonials = [
 const TestimonialSection = () => {
   return (
     <section className='md:px-20 px-10 py-20'>
-        <div className='text-[#33ACFF] space-y-10'>
-            {Testimonials.map((testimonial, index) => (
-                <div key={index} className='rounded-lg border space-y-5 border-[#0097FF] py-7 px-10'>
-                    <Image src={`/images/comma.png`} width={100} height={100} alt='comma' />
-                    <p>{testimonial.paragraph}</p>
-                    <div className='flex gap-x-2 '>
-                        <div className='bg-[#B4E0FF] rounded-full p-6 w-fit h-fit'></div>
-                        <div>
-                            <h2 className='seep-text-color text-2xl font-bold'>{testimonial.name}</h2>
-                            <span>{testimonial.role}</span>
+        <FadeInSection direction={`up`}>
+            <div className='text-[#33ACFF] space-y-10'>
+                {Testimonials.map((testimonial, index) => (
+                    <div key={index} className='rounded-lg border space-y-5 border-[#0097FF] py-7 px-10'>
+                        <Image src={`/images/comma.png`} width={100} height={100} alt='comma' />
+                        <p>{testimonial.paragraph}</p>
+                        <div className='flex gap-x-2 '>
+                            <div className='bg-[#B4E0FF] rounded-full p-6 w-fit h-fit'></div>
+                            <div>
+                                <h2 className='seep-text-color text-2xl font-bold'>{testimonial.name}</h2>
+                                <span>{testimonial.role}</span>
+                            </div>
                         </div>
+
                     </div>
 
-                </div>
+                ))}
 
-            ))}
-
-        </div>
+            </div>  
+        </FadeInSection>
 
       
     </section>
