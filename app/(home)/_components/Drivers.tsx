2125d57d3d555b191e024f8a362097494d60312d
Austinobravo <Austinobravo@gmail.com>
2024-07-22 02:50:05 +0100

    corrected the core drivers

--- app/(home)/_components/Drivers.tsx
+++ app/(home)/_components/Drivers.tsx
@@ -44,11 +44,20 @@ const Drivers = () => {
   return (
     <section className='py-16 '>
         <FadeInSection direction={`up`}>
-            <h2 className='py-10 text-center text-4xl seep-text-color hover:underline font-bold'>Core Drivers</h2>
-            <div className='lg:flex hidden  gap-14 justify-center items-center w-full mx-auto'>
-                {imagesAndText.map((item, index) => (
-                    <div key={index} className='flex flex-col items-center justify-center space-y-3 cursor-pointer hover:bg-black/10 hover:rounded-lg p-2'>
+            <h2 className='py-10 text-center text-4xl seep-text-color font-bold'>Core Drivers</h2>
+            <div className={`lg:flex gap-x-60 hidden basis-1/2 justify-center items-center w-full mx-auto`}>
+                {imagesAndText.slice(0,3).map((item, index) => (
+                    <div key={index} className='flex flex-col items-center justify-center space-y-3 p-2'>
                         <Image src={`${item.image}`} width={40} height={100} alt={item.name}/>
+                        <span className='text-2xl seep-text-color hover:underline font-bold'>{item.name}</span>
+                    </div>
+                ))}
+
+            </div>
+            <div className={`lg:flex gap-x-64 pt-10 hidden basis-1/2 justify-center items-center w-full mx-auto`}>
+                {imagesAndText.slice(3).map((item, index) => (
+                    <div key={index} className='flex flex-col items-center justify-center space-y-3 p-2'>
+                        <Image src={`${item.image}`} width={50} height={100} alt={item.name}/>
                         <span className='text-2xl seep-text-color hover:underline font-bold'>{item.name}</span>
                     </div>
                 ))}
