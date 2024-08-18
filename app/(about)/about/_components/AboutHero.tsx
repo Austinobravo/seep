d187715b57a313e7bf40a64dda260f5fd6cd5a89
Austinobravo <Austinobravo@gmail.com>
2024-08-18 07:39:02 +0100

    About image screen small

--- app/(about)/about/_components/AboutHero.tsx
+++ app/(about)/about/_components/AboutHero.tsx
@@ -5,8 +5,8 @@ const AboutHero = () => {
   return (
     <section className='md:px-20 px-10 space-y-10'>
       <FadeInSection direction={`up`}>
-        <div className='w-full lg:bg-contain bg-cover bg-no-repeat md:bg-right-top bg-center h-[600px] rounded-2xl text-white' style={{backgroundImage: `url(/images/about.png)`}}>
-            <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent h-[600px] rounded-2xl flex flex-col md:pl-10 pl-5 justify-center space-y-5'>
+        <div className='w-full lg:bg-contain bg-cover bg-no-repeat md:bg-right-top bg-center md:h-[600px] h-[480px] rounded-2xl text-white about-hero-image'>
+            <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent md:h-[600px] h-[480px] rounded-2xl flex flex-col md:pl-10 pl-5 justify-center space-y-5'>
                 <h1 className='md:text-6xl text-3xl leading-relaxed md:w-[600px]'>About Us</h1>
             </div>
         </div> 
