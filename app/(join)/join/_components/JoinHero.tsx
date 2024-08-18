077f76c2c41f2be311e10433a3c5fe0f309bb229
Austinobravo <Austinobravo@gmail.com>
2024-08-18 07:47:53 +0100

    Added major small screen sizes

--- app/(join)/join/_components/JoinHero.tsx
+++ app/(join)/join/_components/JoinHero.tsx
@@ -8,8 +8,8 @@ const JoinHero = () => {
     <>
     <section className='md:px-20 px-10 space-y-10'>
       <FadeInSection direction={`up`}>
-        <div className='w-full bg-cover bg-no-repeat md:bg-right-top bg-center h-[600px] text-white rounded-2xl' style={{backgroundImage: `url(/images/join.png)`}}>
-            <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent h-[600px] rounded-2xl flex flex-col md:pl-10 pl-5 justify-center space-y-5'>
+        <div className='w-full bg-cover bg-no-repeat md:bg-right-top bg-center md:h-[600px] h-[480px] text-white rounded-2xl' style={{backgroundImage: `url(/images/join.png)`}}>
+            <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent md:h-[600px] h-[480px] rounded-2xl flex flex-col md:pl-10 pl-5 justify-center space-y-5'>
                 <h1 className='md:text-6xl text-3xl  leading-relaxed md:w-[600px]'>Join Us.</h1>
             </div>
         </div> 
--- app/(support)/support/_components/SupportHero.tsx
+++ app/(support)/support/_components/SupportHero.tsx
@@ -7,8 +7,8 @@ const SupportHero = () => {
   return (
     <section className='md:px-20 px-10 space-y-10'>
       <FadeInSection direction={`up`}>
-        <div className='w-full bg-cover md:bg-right-top bg-center h-[600px] rounded-2xl text-white' style={{backgroundImage: `url(/images/mission_2.png)`}}>
-            <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent h-[600px] rounded-2xl flex flex-col md:pl-10 pl-5 md:justify-center justify-end pb-16 space-y-5'>
+        <div className='w-full bg-cover md:bg-right-top bg-center md:h-[600px] h-[480px] rounded-2xl text-white' style={{backgroundImage: `url(/images/mission_2.png)`}}>
+            <div className='bg-gradient-to-r from-[#0097FF] via-transparent to-transparent md:h-[600px] h-[480px] rounded-2xl flex flex-col md:pl-10 pl-5 justify-center space-y-5'>
                 <h1 className='md:text-6xl text-3xl  leading-relaxed md:w-[600px]'>Support Us.</h1>
             </div>
         </div>       
